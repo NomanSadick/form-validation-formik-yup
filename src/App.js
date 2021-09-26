1669adce4c63149f98188b06c0f93b62ec469e33
NomanSadick <nomansadickjs@gmail.com>
2021-09-26 09:03:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import rocketImg from '../src/image/rocket.png'
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+    <div className="row">
+      <div className="col-md-5">
+        <SignUp></SignUp>
+      </div>
+      <div className="col-md-7 my-auto">
+        <img className="img-fluid w-100" src={rocketImg} alt=""/>
+      </div>
     </div>
+  </div>
   );
 }
 
